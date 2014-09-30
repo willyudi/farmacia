84110e0f18d91408e4c00d60897262d1318f9f30
Williano Serpa <willyudi@gmail.com>
2014-09-29 22:02:42 -0300

    Usando Janelas TK

--- principal.py
+++ principal.py
@@ -1,4 +1,14 @@
 __author__ = 'williano'
-import sys;
-print "Hello World!";
-print "Segunda linha de teste";
+from Tkinter import *
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.msg = Label(self, text="Williano Serpa")
+        self.msg.pack ()
+        self.bye = Button (self, text="Adeus", command=self.quit)
+        self.bye.pack ()
+        self.pack()
+
+app = Application()
+mainloop()
