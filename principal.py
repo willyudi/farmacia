1e3ab72ce8a0c349bac59ae5a2d1c711578a7ee8
Williano Serpa <willyudi@gmail.com>
2014-09-30 22:29:41 -0300

    Usando Janelas TK

--- principal.py
+++ principal.py
@@ -1,10 +1,16 @@
 __author__ = 'williano'
 from Tkinter import *
+import psycopg2
 
 class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.msg = Label(self, text="Williano Serpa")
+
+        con = psycopg2.connect(host='localhost', user='williano', password='willy01',dbname='dbminhafarmacia')
+        c = con.cursor()
+        c.execute('SELECT * FROM tbtipo')
+
+        self.msg = Label(self, text=c.fetchall())
         self.msg.pack ()
         self.bye = Button (self, text="Adeus", command=self.quit)
         self.bye.pack ()
